bcef4fc3ae9cdc4f6d4fa7a3884971a5dd921213
Adrian Lantos <lantosadrian@gmail.com>
2022-07-06 14:39:02 +0300

    Homework Curs 11

--- Curs11/src/Exercises/Main.java
+++ Curs11/src/Exercises/Main.java
@@ -10,11 +10,11 @@ public class Main {
 //      Cea mai mare nota este 17. Nu stiu daca este o greseala pe care trebuia sa o prindem sau daca ai vrut doar sa ai cea mai mare nota clara
         report.writeReport();
 
-        System.out.println("Average grade for Computer Science is " + classroom.getAverageGrade("Computer Science"));
+        System.out.println("Average grade for Computer Science is " + String.format("%.2f", classroom.getAverageGrade("Computer Science")));
         System.out.println("Best grade for Physics is " + classroom.getMaxGrade("Physics"));
         System.out.println("Worst Grade for History is " + classroom.getWorstGrade("History"));
         System.out.println("Best overall grade is " + classroom.getMaxGrade());
         System.out.println("Grades for Mathematics are " + classroom.getGradesForDiscipline("Mathematics"));
-        System.out.println("Duilio Marius has the folowing grades " + classroom.getGradesForStudent("Duilio Marius"));
+        System.out.println("Duilio Marius has the following grades " + classroom.getGradesForStudent("Duilio Marius"));
     }
 }
