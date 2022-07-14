fa39f2865a07126cf49073317adc15f624e169ec
Adrian Lantos <lantosadrian@gmail.com>
2022-07-14 13:28:59 +0300

    Homework Curs 12

--- Curs12/src/ExtraMileDebug/Main.java
+++ Curs12/src/ExtraMileDebug/Main.java
@@ -17,11 +17,22 @@ public class Main {
         }
     }
 
-    public static boolean isPrime(int n) {
+    public static boolean isPrime2(int n) {
         if (n <= 1) {
             return false;
         }
         for (int i = 2; i <= n/2; ++i ) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i += 2) {
             if (n % i == 0) {
                 return false;
             }
